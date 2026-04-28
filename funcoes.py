27f1dbf2b30d47b0dd0d438332ade670ad7e4f09
STV <136649122+Estevao67@users.noreply.github.com>
2026-04-27 21:04:52 -0400

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,54 @@ def salvar_json(arquivo, dados):
     with open(arquivo, "w") as f:
         json.dump(dados, f, indent=2)
 
+import json
+
+def ler_json(arquivo):
+    with open(arquivo, "r") as f:
+        json.load(f)
+
+def salvar_json(arquivo, dados):
+    with open(arquivo, "w") as f:
+        json.dump(dados, f, indent=2)
+
+def menu_usuarioAdmin():
+    
+    print(" 1 - Cadastrar usuários do sistema ")
+    print(" 2 - Editar usuários ")
+    print(" 3 - Excluir usuários ")
+    print(" 4 - Resetar senha de usuários ")
+    print(" 5 - Listar todos os usuários ")
+
+def menu_medicosAdmin():
+    print(" 1 - Cadastrar médicos ")
+    print(" 2 - Editar médicos ")
+    print(" 3 - Excluir médicos cadastrados ")
+    print(" 4 - Listar médicos cadastrados ")
+
+def menu_pacientesAdmin():
+    print(" 1 - Visualizar todos os pacientes ")
+    print(" 2 - Buscar pacientes ")
+    print(" 3 - Ver histórico completo ")
+
+def menu_consultasAdmin():
+    print(" 1 - Visualizar todas as consultas ")
+    print(" 2 - Consultar agenda geral ")
+
+def menu_relatoriosAdmin():
+    print(" 1 - Total de consultas realizadas por período ")
+    print(" 2 - Total de consultas canceladas ")
+    print(" 3 - Quantidade de pacientes ")
+    print(" 4 - Quantidade de médicos ativos ")
+    print(" 5 - Consultas por médico ")
+    print(" 6 - Atendimentos realizados no dia ")
+    print(" 7 - Pacientes mais atendidos")
+
+def menu_consultas():
+    print(" 1 - Marcar consulta para médico específico ")
+    print(" 2 - Escolher data e horário ")
+    print(" 3 - Reagendar consulta ")
+    print(" 4 - Cancelar consulta ")
+    print(" 5 - Confirmar presença do paciente ")
+    print(" 6 - Listar todas as consultas do dia ")
+    print(" 7 - Listar consultas futuras ")
+
